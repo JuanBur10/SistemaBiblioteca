8e9d6b34b2c250f2607cc7efe43cb6895d8a8ae3
JuanBur10 <juanjosebur12@gmail.com>
2026-03-25 22:10:42 -0500

    Busquedas y reportes

--- Blblioteca/Program.cs
+++ Blblioteca/Program.cs
@@ -83,6 +83,17 @@ class Program
             EjecutarAccion($"Operación de préstamo: {op}");
     }
 
+     // --- seccion 4 , busquedas y reportes ---
+    static void MenuReportes()
+    {
+        Console.Clear();
+        Console.WriteLine(">> BÚSQUEDAS Y REPORTES");
+        Console.WriteLine("4.1 Buscar libro (Título/Autor/ISBN/Cat)\n4.2 Buscar usuario\n4.3 Reportes generales\n0. Volver");
+
+        string op = Console.ReadLine() ?? "";
+        if (op != "0") EjecutarAccion("Generando reporte/búsqueda solicitada...");
+    }
+
 
     // --- HELPERS ---
     static void EjecutarAccion(string mensaje)
