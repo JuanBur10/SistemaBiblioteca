bd4a53344bee109da0b06608df95219e2af56079
JuanBur10 <juanjosebur12@gmail.com>
2026-03-25 21:54:59 -0500

    funcionalidad de prestamos

--- Blblioteca/Program.cs
+++ Blblioteca/Program.cs
@@ -67,6 +67,22 @@ class Program
             EjecutarAccion($"Ejecutando operación de usuarios: {op}");
     }
 
+      // --- seccion 4 de prestamos ---
+    static void MenuPrestamos()
+    {
+        Console.Clear();
+        Console.WriteLine(">> GESTIÓN DE PRÉSTAMOS");
+        Console.WriteLine("3.1 Crear préstamo\n3.2 Listar préstamos\n3.3 Ver detalle\n3.4 Registrar devolución\n3.5 Eliminar préstamo\n0. Volver");
+
+        string op = Console.ReadLine() ?? "";
+        if (op == "3.1")
+            EjecutarAccion("Validando disponibilidad de libro y estado de usuario...");
+        else if (op == "3.4")
+            EjecutarAccion("Procesando devolución y actualizando stock...");
+        else if (op != "0")
+            EjecutarAccion($"Operación de préstamo: {op}");
+    }
+
 
     // --- HELPERS ---
     static void EjecutarAccion(string mensaje)
