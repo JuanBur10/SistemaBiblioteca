b71dbdb65444828e3bdcd320619de6c38eb38566
JuanBur10 <juanjosebur12@gmail.com>
2026-03-25 19:29:56 -0500

    Menu de la biblioteca

--- Blblioteca/Program.cs
+++ Blblioteca/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿namespace Biblioteca;
+
+class Program
+{
+    static void Main()
+    {
+        MenuPrincipal();
+    }
+    static void MenuPrincipal()
+        {
+        bool salir = false;
+        while (!salir)
+        {
+            Console.Clear();
+            Console.WriteLine("     SISTEMA DE GESTIÓN BIBLIOTECARIA   ");
+            Console.WriteLine("1. Libros");
+            Console.WriteLine("2. Usuarios");
+            Console.WriteLine("3. Préstamos");
+            Console.WriteLine("4. Búsquedas y Reportes");
+            Console.WriteLine("5. Guardar / Cargar Datos");
+            Console.WriteLine("0. Salir");
+            Console.Write("\nSeleccione una opción: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1": MenuLibros(); break;
+                case "2": MenuUsuarios(); break;
+                case "3": MenuPrestamos(); break;
+                case "4": MenuReportes(); break;
+                case "5": MenuDatos(); break;
+                case "0": 
+                    Console.Write("¿Guardar antes de salir? (S/N): ");
+                    if (Console.ReadLine()?.ToUpper() == "S") EjecutarAccion("Guardando cambios y cerrando sesión...");
+                    salir = true; 
+                    break;
+                default: MensajeError(); break;
+            }
+        }
+    }
+
+    // --- HELPERS ---
+    static void EjecutarAccion(string mensaje)
+    {
+        Console.WriteLine($"\n[PROCESO]: {mensaje}");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+    }
+
+    static void MensajeError()
+    {
+        Console.WriteLine("\n[!] Opción inválida.");
+        Thread.Sleep(800);
+    }
+
+}
