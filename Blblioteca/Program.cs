c987748d700140652e5a6eacdd2bd6bdc28c26f3
JuanBur10 <juanjosebur12@gmail.com>
2026-03-25 19:50:16 -0500

    Seccion de libros

--- Blblioteca/Program.cs
+++ Blblioteca/Program.cs
@@ -1,4 +1,4 @@
-﻿﻿namespace Biblioteca;
+﻿namespace Biblioteca;
 
 class Program
 {
@@ -37,6 +37,22 @@ class Program
             }
         }
     }
+// --- Seccion de libros
+    static void MenuLibros()
+    {
+        Console.Clear();
+        Console.WriteLine(">> GESTIÓN DE LIBROS");
+        Console.WriteLine("1.1 Registrar libro\n1.2 Listar libros\n1.3 Ver detalle (ID/ISBN)\n1.4 Actualizar libro\n1.5 Eliminar libro\n0. Volver");
+        
+        string op = Console.ReadLine() ?? "";
+        if (op == "1.2") 
+            EjecutarAccion("Submenú: Listar (Todos / Disponibles / Prestados)");
+        else if (op == "1.4") 
+            EjecutarAccion("Submenú: Editar (Título / Autor / Año / Categoría)");
+        else if (op != "0")
+            EjecutarAccion($"Ejecutando operación de libros: {op}");
+    }
+
 
     // --- HELPERS ---
     static void EjecutarAccion(string mensaje)
