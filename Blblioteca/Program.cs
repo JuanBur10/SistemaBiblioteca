87f89b8a04a705e21a5e0fadb9f7c2ac969fbd7f
JuanBur10 <juanjosebur12@gmail.com>
2026-03-25 21:32:13 -0500

    Funcionalidad de usuarios

--- Blblioteca/Program.cs
+++ Blblioteca/Program.cs
@@ -53,6 +53,20 @@ class Program
             EjecutarAccion($"Ejecutando operación de libros: {op}");
     }
 
+      // ---Seccion de usuarios---
+    static void MenuUsuarios()
+    {
+        Console.Clear();
+        Console.WriteLine(">> GESTIÓN DE USUARIOS");
+        Console.WriteLine("2.1 Registrar usuario\n2.2 Listar usuarios\n2.3 Ver detalle\n2.4 Actualizar\n2.5 Eliminar\n0. Volver");
+
+        string op = Console.ReadLine() ?? "";
+        if (op == "2.4")
+            EjecutarAccion("Submenú: Editar (Nombre / Contacto / Estado Activo)");
+        else if (op != "0")
+            EjecutarAccion($"Ejecutando operación de usuarios: {op}");
+    }
+
 
     // --- HELPERS ---
     static void EjecutarAccion(string mensaje)
